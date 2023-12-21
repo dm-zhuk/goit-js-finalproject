aa440afc565dfc89b6247a1523142ddeb234960c
dm-zhuk <140021292+dm-zhuk@users.noreply.github.com>
2023-12-21 15:20:32 +0200

    01-lightbox.html, 02-lightbox.html, 01-lightbox.js, 02-lightbox.js update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,70 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+/* Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+Реалізація делегування на ul.gallery і отримання url великого зображення.
+Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
+Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
+Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
+Розмітка елемента галереї
+Посилання на оригінальне зображення повинно зберігатися в data-атрибуті source на елементі <img>, і вказуватися в href посилання. Не додавай інші HTML теги або CSS класи, крім тих, що містяться в цьому шаблоні.
+
+<li class="gallery__item">...
+
+Зверни увагу на те, що зображення обгорнуте посиланням, отже по кліку за замовчуванням користувач буде перенаправлений на іншу сторінку. Заборони цю поведінку за замовчуванням.
++Закриття з клавіатури - Додай закриття модального вікна після натискання клавіші Escape. Зроби так, щоб прослуховування клавіатури було тільки доти, доки відкрите модальне вікно. Бібліотека basicLightbox містить метод для програмного закриття модального вікна. */
+
+import { galleryItems } from "./gallery-items.js";
+
+const galleryList = document.querySelector(".gallery");
+
+function renderGallery(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+        </a>
+      </li>
+    `
+    )
+    .join("");
+}
+
+galleryList.insertAdjacentHTML("afterbegin", renderGallery(galleryItems));
+galleryList.addEventListener("click", handlerGalleryClick);
+
+function handlerGalleryClick(event) {
+  event.preventDefault();
+
+  if (event.currentTarget == event.target) {
+    return;
+  }
+
+  if (event.target.closest(".gallery__item")) {
+    const largeImageSrc = event.target.dataset.source;
+
+    // Initialize BasicLightbox
+    const lightbox = basicLightbox.create(
+      `
+      <img src="${largeImageSrc}" alt="" />
+    `
+    );
+
+    const closeLightbox = () => lightbox.close();
+    const handleKeyDown = (event) => {
+      if (event.key === "Escape") {
+        closeLightbox();
+        window.removeEventListener("keydown", handleKeyDown);
+      }
+    };
+
+    lightbox.show(() => {
+      console.log("lightbox now visible");
+      window.addEventListener("keydown", handleKeyDown);
+    });
+  }
+}
 
 console.log(galleryItems);
+
+// console.log(basicLightbox);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+/* Зроби таку саму галерею як в першому завданні, але використовуючи бібліотеку SimpleLightbox, яка візьме на себе обробку кліків по зображеннях, відкриття і закриття модального вікна, а також гортання зображень за допомогою клавіатури.
+    <li class="gallery__item">
+   <a class="gallery__link" href="large-image.jpg">
+      <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+   </a>
+</li>
+Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+Підключення скрипту і стилів бібліотеки, використовуючи CDN сервіс cdnjs: simple-lightbox.min.js і simple-lightbox.min.css.
+Ініціалізація бібліотеки після створення і додання елементів галереї у ul.gallery. Для цього ознайомся з документацією SimpleLightbox секції «Usage» і «Markup».
+Подивися в документації секцію «Options» і додай відображення підписів до зображень з атрибута alt. Нехай підпис буде знизу і з'являється через 250 мілісекунд після відкриття зображення. */
+
+import { galleryItems } from "./gallery-items.js";
+
+const galleryList = document.querySelector(".gallery");
+
+function renderGallery(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>
+    `
+    )
+    .join("");
+}
+
+galleryList.insertAdjacentHTML("afterbegin", renderGallery(galleryItems));
+galleryList.addEventListener("click", handlerGalleryClick);
+
+function handlerGalleryClick(event) {
+  event.preventDefault();
+}
+
+// Initialize SimpleLightbox
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
